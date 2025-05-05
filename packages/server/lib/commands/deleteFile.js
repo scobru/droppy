da66e1f8466f1b9efd1c7c53748ceb1571682329
Mark Hughes <markwilliamhughes1@gmail.com>
2025-05-05 22:01:49 +1000

    feat(#16): add foundation for multi select

--- packages/server/lib/commands/deleteFile.js
+++ packages/server/lib/commands/deleteFile.js
@@ -3,22 +3,38 @@ import log from "../services/log.js";
 
 export default {
   handler: async ({ validatePaths, sid, config, msg, ws, vId, sendError }) => {
+    if (!Array.isArray(msg.data)) {
+      sendError(sid, vId, "Invalid data");
+      return;
+    }
+
     if (config.readOnly) {
       log.info(ws, null, `Prevent deleting read-only file: ${msg.data}`);
       return sendError(sid, vId, "Files are read-only");
     }
 
-    if (!validatePaths(msg.data, msg.type, ws, sid, vId)) {
-      return;
-    }
-    log.info(ws, null, `Deleting: ${msg.data}`);
+    for (const path of msg.data) {
+      if (typeof path !== "string") {
+        sendError(sid, vId, "Invalid data");
+        return;
+      }
 
-    try {
-      await filetree.del(msg.data);
-    } catch (err) {
-      log.info(ws, null, `Error deleting file: ${msg.data}`);
-      log.error(err);
-      return sendError(sid, vId, "Error deleting file");
+      if (!validatePaths(path, msg.type, ws, sid, vId)) {
+        sendError(sid, vId, `No permission to delete file: ${path}`);
+        return;
+      }
+
+      log.info(ws, null, `Deleting: ${path}`);
+
+
+      try {
+        await filetree.del(path);
+      } catch (err) {
+        log.info(ws, null, `Error deleting file: ${path}`);
+        log.error(err);
+        sendError(sid, vId, `Error deleting: ${path}`);
+        continue;
+      }
     }
   },
 };
